e59b4141a8a2002812c34c7134de27979a59643c
KirylMi <xxxdendelxxx@gmail.com>
2021-06-22 20:59:10 +0300

    build time fixed lol

--- dockerInnerService/simpleServer.cpp
+++ dockerInnerService/simpleServer.cpp
@@ -2,16 +2,36 @@
 
 #include <nghttp2/asio_http2_server.h>
 
+#include "document.h"
+#include "writer.h"
+#include "stringbuffer.h"
+
 using namespace nghttp2::asio_http2;
 using namespace nghttp2::asio_http2::server;
+
+using namespace rapidjson;
 
 int main(int argc, char *argv[]) {
   boost::system::error_code ec;
   http2 server;
 
-  server.handle("/", [](const request &req, const response &res) {
+  // 1. Parse a JSON string into DOM.
+  const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+  Document d;
+  d.Parse(json);
+ 
+  // 2. Modify it by DOM.
+  Value& s = d["stars"];
+  s.SetInt(s.GetInt() + 1);
+ 
+  // 3. Stringify the DOM
+  StringBuffer buffer;
+  Writer<StringBuffer> writer(buffer);
+  d.Accept(writer);
+
+  server.handle("/", [&buffer](const request &req, const response &res) {
     res.write_head(200);
-    res.end("Some future json stuff");
+    res.end(buffer.GetString());
   });
 
   if (server.listen_and_serve(ec, "0.0.0.0", "3000")) {
