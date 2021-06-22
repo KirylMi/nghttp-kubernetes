30514ae5a1dd5a3b8094b660f74ad4cd1de935b0
KirylMi <xxxdendelxxx@gmail.com>
2021-06-22 22:19:54 +0300

    temp commit. Inner service crashes rn, fix incoming

--- dockerInnerService/simpleServer.cpp
+++ dockerInnerService/simpleServer.cpp
@@ -16,22 +16,34 @@ int main(int argc, char *argv[]) {
   http2 server;
 
   // 1. Parse a JSON string into DOM.
-  const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-  Document d;
-  d.Parse(json);
+  //const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+  //Document d;
+  //d.Parse(json);
  
   // 2. Modify it by DOM.
-  Value& s = d["stars"];
-  s.SetInt(s.GetInt() + 1);
+  //Value& s = d["stars"];
+  //s.SetInt(s.GetInt() + 1);
  
   // 3. Stringify the DOM
-  StringBuffer buffer;
-  Writer<StringBuffer> writer(buffer);
-  d.Accept(writer);
+  //StringBuffer buffer;
+  //Writer<StringBuffer> writer(buffer);
+  //d.Accept(writer);
 
-  server.handle("/", [&buffer](const request &req, const response &res) {
+  server.handle("/", [](const request &req, const response &res) {
+    req.on_data([&res](const uint8_t *data, std::size_t len) {
+      Document d;
+      //monstrosity
+      char *cptr = reinterpret_cast<char*>(const_cast<uint8_t*>(data));
+      d.Parse(cptr);
+      std::cerr<<"data received: "<<data;
+      if(d.HasMember("testRequest")){
+        res.write_head(200);
+        res.end("{\"testResponce\":\"success\"}");
+      }      
+    });
+    //res.write_head(500);
     res.write_head(200);
-    res.end(buffer.GetString());
+    res.end("something is wrong with the request body");
   });
 
   if (server.listen_and_serve(ec, "0.0.0.0", "3000")) {
